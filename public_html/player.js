224ba654ba991afa05aa97eef1d4d33b87c75bbd
teeveeJS <teemu.eemeli99@gmail.com>
2016-09-17 18:19:57 -0700

    idk

--- public_html/player.js
+++ public_html/player.js
@@ -10,16 +10,13 @@ function PlayerCharacter(name, /*hp,*/ speed, x, y, f){
     this.loadCharacter = function(){
         var p = document.getElementById("player");   
         if(p === null){
-            //p = document.createElement("DIV");
             p = document.createElement("IMG");
             p.setAttribute("src", "pacman.gif");
             document.body.appendChild(p);
             p.setAttribute("id", "player");
-            //p.style.backgroundColor = "red";
             p.style.height = "50px";
             p.style.width = "50px";
             p.style.zIndex = "999";
-            //p.style.borderRadius = "100%";
         }
         p.style.marginTop = this.y + "px";
         p.style.marginLeft = this.x + "px";
