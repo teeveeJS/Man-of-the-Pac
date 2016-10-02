a71ccacf7698991e76da0d566194f79f6594580c
teeveeJS <teemu.eemeli99@gmail.com>
2016-10-02 13:09:24 -0700

    multiplayer ghost gen

--- public_html/distance.js
+++ public_html/distance.js
@@ -1,6 +1,6 @@
-function distance(player, ghost){
-    var dx = player.x - ghost.x;
-    var dy = player.y - ghost.y;
+function distance(px, py, gx, gy){
+    var dx = px - gx;
+    var dy = py - gy;
     var dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
     return dist;
 }
--- public_html/ghostGen.js
+++ public_html/ghostGen.js
@@ -1,8 +1,19 @@
-function ghostGen(p, ghost_arr){
+function ghostGen(p_arr, ghost_arr){
     ghost_speed++;
     var ghost_name = "ghost" + ghost_arr.length;
-    var ghost_x = (parseInt(p.x) > 280) ? 100 : 500;
-    var ghost_y = (parseInt(p.y) > 280) ? 100 : 500;
-    ghost_arr[ghost_arr.length] = new ghost(ghost_name, ghost_x, ghost_y, ghost_speed, 100).loadGhost();
+    var xy = ghostRandCoord(p_arr);
+    ghost_arr[ghost_arr.length] = new ghost(ghost_name, xy[0], xy[1], ghost_speed, 100).loadGhost();
 }
 
+function ghostRandCoord(p_arr){
+    var ghost_x = Math.floor(Math.random()*450) + 50;
+    var ghost_y = Math.floor(Math.random()*450) + 50;
+    for(i=0;i<p_arr.length;i++){
+        if(distance(p_arr.x, p_arr.y, ghost_x, ghost_y) < 100){
+            ghostRandCoord(p_arr);
+            break;
+        } else {
+            return [ghost_x, ghost_y];
+        }
+    }
+}
